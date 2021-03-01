44718b5803e218438f8900aa377525e2de17fafe
SantiVi33 <santi.vinuela@hotmail.com>
2021-03-01 16:16:20 -0300

    Agregador de componentes y prueba en el Localhost

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PortadaComponent } from './portada/portada.component';
+import { CabezeraComponent } from './cabezera/cabezera.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { GaleriaComponent } from './galeria/galeria.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortadaComponent,
+    CabezeraComponent,
+    FooterComponent,
+    ContactoComponent,
+    GaleriaComponent
   ],
   imports: [
     BrowserModule,
